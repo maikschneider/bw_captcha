3a2b0431be191af92bb08a48d99e223159d02b79
Maik Schneider <m.schneider@blueways.de>
2021-10-05 10:47:51 +0200

    feat: create new captcha

--- Classes/Controller/CaptchaController.php
+++ Classes/Controller/CaptchaController.php
@@ -2,6 +2,9 @@
 
 namespace Blueways\BwCaptcha\Controller;
 
+use Gregwar\Captcha\CaptchaBuilder;
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class CaptchaController extends ActionController
@@ -16,7 +19,18 @@ class CaptchaController extends ActionController
 
     public function refreshAction(string $cacheIdentifier)
     {
+        // create new captcha
+        $builder = new CaptchaBuilder;
+        $builder->build();
+        $captcha = $builder->inline();
+
+        // override captcha secret in cache
+        $phrase = $builder->getPhrase();
+        $cache = GeneralUtility::makeInstance(CacheManager::class)->getCache('bwcaptcha');
+        $cache->set($cacheIdentifier, $phrase, [], 86400);
+
+        // return new inline captcha
         $this->view->setVariablesToRender(['captcha']);
-        $this->view->assign('captcha', ['captcha' => $cacheIdentifier]);
+        $this->view->assign('captcha', ['captcha' => $captcha]);
     }
 }
