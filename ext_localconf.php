4acb915f30cb24f7bcee553c1975b5e8dfb83ae9
Maik Schneider <mas@xima.de>
2022-10-16 14:12:29 +0200

    chore: replace FQN in ext_localconf.php with imports

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,18 @@
 <?php
 
+use Blueways\BwCaptcha\Controller\CaptchaController;
+use Blueways\BwCaptcha\Hooks\FormElementCaptchaHook;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 defined('TYPO3') or die();
 
 call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
+    ExtensionManagementUtility::addTypoScriptSetup(trim('
             module.tx_form {
                 settings {
                     yamlConfigurations {
@@ -13,19 +22,17 @@ call_user_func(function () {
             }
         '));
 
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
     $iconRegistry->registerIcon(
         't3-form-captcha-element',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bw_captcha/Resources/Public/Images/form-captcha-icon.svg']
     );
 
     // get typo3 version
-    $verionNumberUtility = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Utility\VersionNumberUtility::class);
+    $verionNumberUtility = GeneralUtility::makeInstance(VersionNumberUtility::class);
     $version = $verionNumberUtility->convertVersionStringToArray($verionNumberUtility->getNumericTypo3Version());
-    $captchaControllerName = $version['version_main'] > 9 ? \Blueways\BwCaptcha\Controller\CaptchaController::class : 'Captcha';
+    $captchaControllerName = $version['version_main'] > 9 ? CaptchaController::class : 'Captcha';
     $extensionName = $version['version_main'] > 11 ? 'BwCaptcha' : 'Blueways.BwCaptcha';
 
     // register cache table
@@ -34,11 +41,10 @@ call_user_func(function () {
     }
 
     // register hook for captcha generation
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeRendering'][1630333427]
-        = \Blueways\BwCaptcha\Hooks\FormElementCaptchaHook::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeRendering'][1630333427] = FormElementCaptchaHook::class;
 
     // register plugin for captcha refresh endpoint
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         $extensionName,
         'Pi1',
         [
