36d89fed8454b2206783dc1abd31586b140eee24
Maik Schneider <mas@xima.de>
2024-08-01 12:52:25 +0200

    feat: use new release workflow

---
+++ packaging_exclude.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'directories' => [
+        '.build',
+        '.ddev',
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'resources\\/private\\/frontend',
+        'resources\\/private\\/libs\\/build',
+        'tailor-version-upload',
+        'Tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CODE_OF_CONDUCT.md',
+        'codeception.yml',
+        'codecov.yml',
+        'CODEOWNERS',
+        'composer.lock',
+        'CONTRIBUTING.md',
+        'crowdin.yaml',
+        'dependency-checker.json',
+        'docker-compose.yml',
+        'editorconfig',
+        'editorconfig-lint.php',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpstan.php',
+        'phpstan-baseline.neon',
+        'phpunit.functional.xml',
+        'phpunit.unit.xml',
+        'rector.php',
+        'renovate.json',
+        'typoscript-lint.yml',
+    ],
+];
