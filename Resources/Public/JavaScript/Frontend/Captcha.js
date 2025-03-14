b9fe2f6dccfaa1ad91eb4c6d70b9e630121b13d6
Maik Schneider <mas@xima.de>
2025-03-14 14:02:53 +0100

    feat: add init function to window

--- Resources/Public/JavaScript/Frontend/Captcha.js
+++ Resources/Public/JavaScript/Frontend/Captcha.js
@@ -1,56 +1,78 @@
-window.BwCaptcha = function () {
-  document.querySelectorAll('a.captcha__reload:not([data-initialized="true"])').forEach(function (element) {
-    element.addEventListener('click', function (event) {
-      event.preventDefault();
-      const div = event.currentTarget.parentElement;
-      div.classList.add('captcha--reloading', 'captcha--spin');
-      let captchaUrl = event.currentTarget.dataset.url;
-      event.currentTarget.previousElementSibling.setAttribute('src', captchaUrl + (/\?/.test(captchaUrl) ? '&' : '?') + 'now=' + Date.now());
-      setTimeout(function () {
-        div.classList.remove('captcha--spin')
-      }, 400);
+window.BwCaptcha = {
+  init() {
+    this.initReloadButton();
+    this.initImage();
+    this.initAudioButton();
+  },
+
+  initReloadButton() {
+    document.querySelectorAll('a.captcha__reload:not([data-initialized="true"])').forEach(element => {
+      element.addEventListener('click', this.handleReloadClick);
+      element.setAttribute('data-initialized', 'true');
     });
-    element.setAttribute('data-initialized', 'true');
-  })
+  },
 
-  document.querySelectorAll('.captcha img[aria-live="polite"]:not([data-initialized="true"])').forEach(function (element) {
-    element.addEventListener('load', function (event) {
-      const div = event.currentTarget.parentElement;
-      div.classList.remove('captcha--reloading');
-    })
-    element.setAttribute('data-initialized', 'true');
-  })
+  handleReloadClick(event) {
+    event.preventDefault();
+    const div = event.currentTarget.parentElement;
+    div.classList.add('captcha--reloading', 'captcha--spin');
+    const captchaUrl = event.currentTarget.dataset.url;
+    event.currentTarget.previousElementSibling.setAttribute('src', `${captchaUrl}${/\?/.test(captchaUrl) ? '&' : '?'}now=${Date.now()}`);
+    setTimeout(() => div.classList.remove('captcha--spin'), 400);
+  },
 
-  document.querySelectorAll('a.captcha__audio:not([data-initialized="true"])').forEach(function (element) {
-    element.addEventListener('click', function (event) {
-      event.preventDefault();
-      const div = event.currentTarget.parentElement;
-      window.captchaAudio = window.captchaAudio ? window.captchaAudio : new Audio();
-      window.captchaAudio.addEventListener('ended', () => div.classList.remove('captcha--playing'));
-      if (window.captchaAudio.paused) {
-        div.classList.add('captcha--playing');
-        const img = div.querySelector('img');
-        const c = document.createElement('canvas');
-        const ctx = c.getContext('2d');
-        const data = new FormData();
-        c.width = img.naturalWidth;
-        c.height = img.naturalHeight;
-        ctx.drawImage(img, 0, 0);
-        data.append('captchaDataUrl', c.toDataURL());
-        const audioUrl = event.currentTarget.dataset.url;
-        fetch(audioUrl, {method: 'POST', body: data})
-          .then(response => response.blob())
-          .then(blob => {
-            window.captchaAudio.src = window.URL.createObjectURL(blob);
-            window.captchaAudio.play();
-          });
-      } else {
-        window.captchaAudio.pause();
-        div.classList.remove('captcha--playing')
-      }
-    })
-    element.setAttribute('data-initialized', 'true');
-  });
-}
+  initImage() {
+    document.querySelectorAll('.captcha img[aria-live="polite"]:not([data-initialized="true"])').forEach(element => {
+      element.addEventListener('load', this.handleImageLoad);
+      element.setAttribute('data-initialized', 'true');
+    });
+  },
 
-window.BwCaptcha();
+  handleImageLoad(event) {
+    const div = event.currentTarget.parentElement;
+    div.classList.remove('captcha--reloading');
+  },
+
+  initAudioButton() {
+    document.querySelectorAll('a.captcha__audio:not([data-initialized="true"])').forEach(element => {
+      element.addEventListener('click', this.handleAudioClick);
+      element.setAttribute('data-initialized', 'true');
+    });
+  },
+
+  handleAudioClick(event) {
+    event.preventDefault();
+    const div = event.currentTarget.parentElement;
+    window.captchaAudio = window.captchaAudio || new Audio();
+    window.captchaAudio.addEventListener('ended', () => div.classList.remove('captcha--playing'));
+
+    if (window.captchaAudio.paused) {
+      div.classList.add('captcha--playing');
+      window.BwCaptcha.playCaptchaAudio(div, event.currentTarget.dataset.url);
+    } else {
+      window.captchaAudio.pause();
+      div.classList.remove('captcha--playing');
+    }
+  },
+
+  playCaptchaAudio(div, audioUrl) {
+    const img = div.querySelector('img');
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const data = new FormData();
+
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    ctx.drawImage(img, 0, 0);
+    data.append('captchaDataUrl', canvas.toDataURL());
+
+    fetch(audioUrl, {method: 'POST', body: data})
+      .then(response => response.blob())
+      .then(blob => {
+        window.captchaAudio.src = window.URL.createObjectURL(blob);
+        window.captchaAudio.play();
+      });
+  }
+};
+
+window.BwCaptcha.init();
